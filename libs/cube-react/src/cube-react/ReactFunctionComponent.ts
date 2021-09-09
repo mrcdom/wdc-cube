d6f3040830ed6ef2adc7a5f5238766411406699c
Marcelo Domingos <mrcdom@gmail.com>
2021-09-09 13:29:32 -0300

    Fix bug attaching update on FC

--- libs/cube-react/src/cube-react/ReactFunctionComponent.ts
+++ libs/cube-react/src/cube-react/ReactFunctionComponent.ts
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Scope, ApplicationPresenter, NOOP_VOID } from 'wdc-cube'
+import { Scope, ApplicationPresenter, NOOP_VOID, Logger } from 'wdc-cube'
+
+const LOG = Logger.get('React.FC')
 
 type ReactType = typeof React
 
@@ -7,13 +9,16 @@ export function bindUpdate<S extends Scope>(react: ReactType, scope: S) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [__, setValue] = react.useState(0)
 
-    if (scope.update == NOOP_VOID) {
+    if (scope.update === NOOP_VOID) {
         scope.update = () => setValue(value => value + 1)
     }
 
-    react.useCallback(() => {
+    react.useEffect(() => {
+        LOG.debug(`scope(${scope.id}).attached`)
+
         return () => {
             scope.update = NOOP_VOID
+            LOG.debug(`scope(${scope.id}).detached`)
         }
     }, [])
 }
@@ -24,22 +29,27 @@ export function getOrCreateApplication<S extends Scope, A extends ApplicationPre
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [__, setValue] = react.useState(0)
 
-    react.useCallback(() => {
+    react.useEffect(() => {
         return () => {
             if (app) {
                 app.scope.update = NOOP_VOID
                 setApp(undefined)
             }
+            LOG.debug(`app.detached`)
         }
     }, [])
 
-    if (!app) {
+    if (app) {
+        if (app.scope.update === NOOP_VOID) {
+            app.scope.update = () => setValue(value => value + 1)
+        }
+        return app
+    } else {
         const instance = factory()
         instance.scope.update = () => setValue(value => value + 1)
         instance.initialize()
         setApp(instance)
+        LOG.debug(`app.attached`)
         return instance
-    } else {
-        return app
     }
 }
