8398b96cbb84bda5aedcf67588dcf666c7db5fc9
Marcelo Domingos <mrcdom@gmail.com>
2021-09-17 18:22:04 -0300

    Gave up on last cube build strategy

--- apps/cube-tutorial/src/Cube.ts
+++ apps/cube-tutorial/src/Cube.ts
@@ -1,4 +1,4 @@
-import { cube, place } from 'wdc-cube'
+import { Place } from 'wdc-cube'
 
 import { TodoMvcPresenter } from './modules/todo-mvc/TodoMvc.presenter'
 import { SubscriptionsPresenter } from './modules/subscriptions/Subscriptions.presenter'
@@ -6,12 +6,13 @@ import { SubscriptionsDetailPresenter } from './modules/subscriptions/detail/Sub
 import { RestrictedPresenter } from './modules/restricted/Restricted.presenter'
 import { Places as p } from './Constants'
 
-export const buildCube = () => {
-    cube(
-        p.todos = place('todos', TodoMvcPresenter),
-        p.subscriptions = place('subscriptions', SubscriptionsPresenter,
-            p.subscriptionsDetail = place('detail', SubscriptionsDetailPresenter)
-        ),
-        p.restricted = place('restricted', RestrictedPresenter)
-    )    
+export function buildCube() {
+    p.todos = Place.create('/todos', TodoMvcPresenter)
+
+    p.subscriptions = Place.create('/subscriptions', SubscriptionsPresenter)
+    {
+        p.subscriptionsDetail = Place.create('subscriptions/detail', SubscriptionsDetailPresenter)
+    }
+
+    p.restricted = Place.create('/restricted', RestrictedPresenter)
 }
